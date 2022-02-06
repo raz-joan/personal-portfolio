f6865a8412e776b405bef75dfa41d6ab9e63c064
Joan Rasmussen <raz.joan@gmail.com>
2022-02-06 11:04:24 -0700

    Fix styling on OtherWork

--- src/Components/OtherWork.js
+++ src/Components/OtherWork.js
@@ -10,19 +10,19 @@ import WheatCover from '../Assets/WheatCover.png'
 const OtherWork = () => {
   return (
     <div className="other-work-container">
-      <a href='https://www.youtube.com/watch?v=Ixqr9e3wCxI'>YouTube Link: How to fold a pocket zine!</a>
+      <a href='https://www.youtube.com/watch?v=Ixqr9e3wCxI' className="work-link">YouTube Link: How to fold a pocket zine!</a>
       <div className="works-container">
-        <div className='booklet'>
-          <img src={breadBookletCover} alt="Bread Booklet Cover"/>
-          <a href={sourdoughBread}>Sourdough Bread Booklet</a>
+        <div className='work'>
+          <img className="work-img" src={breadBookletCover} alt="Bread Booklet Cover"/>
+          <a href={sourdoughBread} className="work-link">Sourdough Bread Booklet</a>
         </div>
-        <div className='booklet'>
-          <img src={starterCover} alt="Sourdough Starter Zine Cover"/>
-          <a href={EveryDaySourdoughStarter}>Sourdough Starter Pocket Zine</a>
+        <div className='work'>
+          <img className="work-img" src={starterCover} alt="Sourdough Starter Zine Cover"/>
+          <a href={EveryDaySourdoughStarter} className="work-link">Sourdough Starter Pocket Zine</a>
         </div>
-        <div className='booklet'>
-          <img src={WheatCover} alt="Wheat Zine Cover"/>
-          <a href={WheatBooklet}>Moxie Wheat Pocket Zine</a>
+        <div className='work'>
+          <img className="work-img" src={WheatCover} alt="Wheat Zine Cover"/>
+          <a href={WheatBooklet} className="work-link">Moxie Wheat Pocket Zine</a>
         </div>
       </div>
     </div>
