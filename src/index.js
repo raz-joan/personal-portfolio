b9f97fa8ea571ed03b4d59cd78ff70986f6bf81e
Joan Rasmussen <raz.joan@gmail.com>
2022-02-19 12:32:23 -0700

    Update browserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import './SassStyles/index.scss';
 import App from './Components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
